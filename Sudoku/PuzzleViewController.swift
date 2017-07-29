b83af03a5e77208cee60ee4f0e1b03b5aa6dac1e
Phanindra Purighalla <phanindra.purighalla@gmail.com>
2017-07-29 23:07:44 +0530

    segues

--- Sudoku/PuzzleViewController.swift
+++ Sudoku/PuzzleViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ViewController.swift
+//  PuzzleViewController.swift
 //  Sudoku
 //
-//  Created by Phanindra purighalla on 24/07/17.
+//  Created by Phanindra purighalla on 28/07/17.
 //  Copyright © 2017 Aarna Solutions Inc. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class PuzzleViewController: UIViewController {
     
     var isEliminatePressed = false
     
@@ -19,14 +19,14 @@ class ViewController: UIViewController {
     var selectedSymbol = ""
     
     var originalMatrix = [["2","7","0","0","0","8","3","0","0"],
-                  ["0","0","0","7","0","0","0","8","0"],
-                  ["0","0","0","0","0","0","0","5","1"],
-                  ["7","0","0","6","0","1","0","9","5"],
-                  ["8","0","0","4","2","3","0","0","7"],
-                  ["1","4","0","5","0","7","0","0","8"],
-                  ["9","1","0","0","0","0","0","0","0"],
-                  ["0","6","0","0","0","5","0","0","0"],
-                  ["0","0","3","8","0","0","0","1","9"]]
+                          ["0","0","0","7","0","0","0","8","0"],
+                          ["0","0","0","0","0","0","0","5","1"],
+                          ["7","0","0","6","0","1","0","9","5"],
+                          ["8","0","0","4","2","3","0","0","7"],
+                          ["1","4","0","5","0","7","0","0","8"],
+                          ["9","1","0","0","0","0","0","0","0"],
+                          ["0","6","0","0","0","5","0","0","0"],
+                          ["0","0","3","8","0","0","0","1","9"]]
     
     var matrix = [["2","7","0","0","0","8","3","0","0"],
                   ["0","0","0","7","0","0","0","8","0"],
@@ -39,14 +39,14 @@ class ViewController: UIViewController {
                   ["0","0","3","8","0","0","0","1","9"]]
     
     var solvedMatrix = [["2","7","9","1","5","8","3","4","6"],
-                  ["3","5","1","7","4","6","9","8","2"],
-                  ["6","8","4","2","3","9","7","5","1"],
-                  ["7","3","2","6","8","1","4","9","5"],
-                  ["8","9","5","4","2","3","1","6","7"],
-                  ["1","4","6","5","9","7","2","3","8"],
-                  ["9","1","8","3","6","2","5","7","4"],
-                  ["4","6","7","9","1","5","8","2","3"],
-                  ["5","2","3","8","7","4","6","1","9"]]
+                        ["3","5","1","7","4","6","9","8","2"],
+                        ["6","8","4","2","3","9","7","5","1"],
+                        ["7","3","2","6","8","1","4","9","5"],
+                        ["8","9","5","4","2","3","1","6","7"],
+                        ["1","4","6","5","9","7","2","3","8"],
+                        ["9","1","8","3","6","2","5","7","4"],
+                        ["4","6","7","9","1","5","8","2","3"],
+                        ["5","2","3","8","7","4","6","1","9"]]
     
     var colArray = [["0","0","0","0","0","0","0","0","0"],
                     ["0","0","0","0","0","0","0","0","0"],
@@ -829,12 +829,12 @@ class ViewController: UIViewController {
         
         populateInitialState()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBOutlet weak var Eliminate: UIButton!
     
     
@@ -858,7 +858,7 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var Nine: UIButton!
-
+    
     @IBAction func NinePressed(_ sender: UIButton) {
         Eight.isEnabled = false
         Seven.isEnabled = false
@@ -971,7 +971,7 @@ class ViewController: UIViewController {
         Eliminate.isEnabled = true
         Erase.isEnabled = true
         Solve.isEnabled = true
-            
+        
         selectedSymbol = "4"
     }
     
@@ -1009,7 +1009,7 @@ class ViewController: UIViewController {
         Eliminate.isEnabled = true
         Erase.isEnabled = true
         Solve.isEnabled = true
-            
+        
         selectedSymbol = "2"
     }
     
@@ -1100,7 +1100,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func unwindToHome(unwindSegue: UIStoryboardSegue) {
-         
+        
     }
 }
+
 
